c4c4a6d76c3d7ffe9617951a97fd7d238528e4c6
Rshrino <tituks4@gmail.com>
2017-12-03 13:40:23 +0200

    Nieko gaidy

--- git bruh.cpp
+++ git bruh.cpp
@@ -1,10 +1,10 @@
-#include "git bruh.h"
-#include "iostream"
+#include <git bruh.h>
+#include <iostream>
 
 using namespace std;
 
 // Global static pointer used to ensure a single instance of the class.
-Singleton* Singleton::m_pInstance = NULL;
+Singleton* Singleton::m_pIntance = NULL;
 
 /** This function is called to create an instance of the class.
 Calling the constructor publicly is not allowed. The constructor
@@ -13,10 +13,10 @@ is private and is only called by this Instance function.
 
 Singleton* Singleton::Instance()
 {
-	if (!Singleton::m_pInstance)   // Only allow one instance of class to be generated.
-		Singleton::m_pInstance = new Singleton;
+	if (!Singleton::m_pIntance)   // Only allow one instance of class to be generated.
+		Singleton::m_pIntance = new Singleton
 
-	return Singleton::m_pInstance;
+	return Singleton::m_pIntance;
 }
 
 void Singleton::writeToLogFile()
